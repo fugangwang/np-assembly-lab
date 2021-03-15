7e739747ea12ea95caf5adf8401737155a3a286b
fugangwang <kevinwangfg@gmail.com>
2021-03-15 15:35:41 -0400

    testing preprocessor

--- bin/test/npassembly_test.py
+++ bin/test/npassembly_test.py
@@ -11,12 +11,12 @@ def test_runPreprocessor(tb):
     t0 = datetime.now()
     func = tb.ref("runPreprocessor")
     func(DEFAULT_NPCHARGE, DEFAULT_LIGAND, DEFAULT_SALT)
-    logf = "preprocessor.log"
-    lammpsf = "in.lammps"
+    logf = "./preprocessor.log"
+    lammpsf = "./in.lammps"
     assert os.path.exists(logf) == 1
     assert os.path.exists(lammpsf) == 1
-    t1 = os.path.getmtime("logf")
-    t2 = os.path.getmtime(lammpsf)
+    t1 = datetime.fromtimestamp(os.path.getmtime(logf))
+    t2 = datetime.fromtimestamp(os.path.getmtime(lammpsf))
     assert (t1>t0 and t2>t0)
     os.remove(logf)
     os.remove(lammpsf)
