a2991ebffb195cc6fc0f5b5efe1b2cdd153b6764
fugangwang <kevinwangfg@gmail.com>
2021-03-22 06:14:04 -0400

    backend test in travis

--- bin/test/npassembly_test.py
+++ bin/test/npassembly_test.py
@@ -26,7 +26,7 @@ def test_backend():
     retcode1, retstr = subprocess.getstatusoutput("make run-preprocessor")
     assert (retcode1 == 0)
     retcode2, retstr = subprocess.getstatusoutput("time lmp < ./in.lammps")
-    assert (retcode2 == 0)
-    #mk1 = "Total # of neighbors"
-    #mk2 = "Ave neighs/atom"
-    #assert ((mk1 in runret) and (mk2 in runret))
+    #assert (retcode2 == 0)
+    mk1 = "Total # of neighbors"
+    mk2 = "Ave neighs/atom"
+    assert ((mk1 in retstr) and (mk2 in retstr))
